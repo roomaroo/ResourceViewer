4c4ebd8917ac1d1828ce4b3a67416dc2619b50af
Olly Maunder <oliver.maunder@dnvgl.com>
2020-06-19 11:23:33 +0100

    Wrote the program

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ResourceViewer
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: ResourceViewer <path to assembly>");
+            }
+
+            try
+            {
+                var assemblyFile = new FileInfo(args[0]);
+                if (!assemblyFile.Exists)
+                {
+                    throw new FileNotFoundException($"Could not find file {assemblyFile.FullName}");
+                }
+            
+                var assembly = Assembly.LoadFrom(assemblyFile.FullName);
+                
+                foreach(var resourceName in assembly.GetManifestResourceNames())
+                {
+                    Console.WriteLine($"  Resource: {resourceName}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.GetType().Name}: {e.Message}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ResourceViewer
 dotnet core console app that lists the resources in an assembly
+
+## Usage
+dotnet run <path to dotnet assembly>
+
+## Credits
+Shamelessy nicked from this StackOverflow answer https://stackoverflow.com/a/6634964
